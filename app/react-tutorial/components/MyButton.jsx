63ef46062704975a8b6a2f3401d75db5d9a6c73e
tchinlapha <tchinlapha@gmail.com>
2024-08-28 16:26:32 +0700

    workshop

---
+++ app/react-tutorial/components/MyButton.jsx
@@ -0,0 +1,8 @@
+"use client";
+import React from 'react'
+
+export default function MyButton(props) {
+  return (
+    <button>{props.name}</button>
+  )
+}
---
+++ app/react-tutorial/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+import React, {useState, useEffect} from "react";
+import MyButton from "./components/MyButton";
+
+export default function page() {
+  
+  return (
+    <div>
+      <MyButton name="ASDSADASD" />
+      <br />
+      <MyButton name="CAT" />
+      <br />
+      <MyButton name="@#!#!@#RWQRWQR" />
+    </div>
+  );
+}
